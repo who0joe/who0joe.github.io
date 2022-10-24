0fbaa65bed12e0f0ada713bf3621b6a8d8d70162
Hooyoung <rocenrol@naver.com>
2022-10-24 15:04:48 +0000

    post-create.sh update

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
+# 동작을 안하는것 같음 뭐가 문제인지
+FILE_PATH="../jekyll/"
 
 # Install the version of Bundler.
-if [ -f Gemfile.lock ] && grep "BUNDLED WITH" Gemfile.lock > /dev/null; then
-    cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs gem install bundler -v
+if [ -f ${FILE_PATH}Gemfile.lock ] && grep "BUNDLED WITH" ${FILE_PATH}Gemfile.lock > /dev/null; then
+    cat ${FILE_PATH}Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs gem install bundler -v
 fi
 
-# If there's a Gemfile, then run `bundle install`
-# It's assumed that the Gemfile will install Jekyll too
-if [ -f Gemfile ]; then
+# # If there's a Gemfile, then run `bundle install`
+# # It's assumed that the Gemfile will install Jekyll too
+if [ -f ${FILE_PATH}Gemfile ]; then
     bundle install
 fi
+
+cd ./jekyll/
